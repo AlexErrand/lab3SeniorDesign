7c9fb80ee4ab8d0a43c572d635ff24e8ab751ff4
rvalle <rogelio-valle@uiowa.edu>
2023-11-22 14:50:31 -0600

    more testing with auth0

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 const express = require("express");
+const { auth } = require("express-oauth2-jwt-bearer");
 const { join } = require("path");
+const authConfig = require("./auth_config.json");
+
 const app = express();
 
-// Serve static assets from the /public folder
+// Serve assets from the /public folder
 app.use(express.static(join(__dirname, "public")));
 
-// Endpoint to serve the configuration file
+// Create the JWT validation middleware
+const checkJwt = auth({
+  audience: authConfig.audience,
+  issuerBaseURL: `https://${authConfig.domain}`
+});
+
+// Create an endpoint that uses the above middleware to
+// protect this route from unauthorized requests
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
+
+// Serve the auth configuration file
 app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
 });
 
-// Serve the index page for all other requests
-app.get("/*", (_, res) => {
-  res.sendFile(join(__dirname, "index.html"));
+// Serve the login page to everything else
+app.get("/*", (req, res) => {
+  res.sendFile(join(__dirname, "login.html"));
 });
 
-// Listen on port 3000
-app.listen(3000, () => console.log("Application running on port 3000"));
+// Error handler
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
+});
+
+module.exports = app;
