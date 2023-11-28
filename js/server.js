c77284fdb5ae0a7df8a5e780e1a12ec310ec5cea
Brandon Cano <brandoncano01@gmail.com>
2023-11-27 18:18:52 -0600

    being more specific

--- js/server.js
+++ js/server.js
@@ -43,8 +43,8 @@ app.use(function(err, req, res, next) {
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-  // res.setHeader('Access-Control-Allow-Origin', 'https://seaman-squad.pages.dev'); // only allow request from our origin
+  //res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Origin', 'https://seaman-squad.pages.dev'); // only allow request from our origin
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
