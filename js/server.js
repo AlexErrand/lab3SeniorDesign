9f8d95cdff8c81c4464bc9cca7b580bdc376b078
Brandon Cano <brandoncano01@gmail.com>
2023-11-27 18:42:34 -0600

    adding another thing

--- js/server.js
+++ js/server.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://seaman-squad.pages.dev'); // only allow request from our origin
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
