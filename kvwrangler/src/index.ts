f7c6cc1a869f4d7f9d1ae9c20a62b77dbd0eda84
Brandon Cano <brandoncano01@gmail.com>
2023-11-27 19:48:13 -0600

    Update index.ts

--- kvwrangler/src/index.ts
+++ kvwrangler/src/index.ts
@@ -37,8 +37,9 @@ export interface Env {
 
 const corsHeaders = {
 	'Access-Control-Allow-Origin': 'https://seaman-squad.pages.dev/',
+	'Access-Control-Allow-Credentials': 'true',
 	'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-	'Access-Control-Allow-Headers': 'Content-Type',
+	'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
   };
   
   function handleOptions(request: Request) {
